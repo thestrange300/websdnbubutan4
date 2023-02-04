34582697c23dfc38f3e9402f75d8629e3c23186c
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-02-04 20:37:32 +0700

    test #2

---
+++ public/js/dropdown.js
@@ -0,0 +1,9 @@
+const dropdown = document.getElementById("dropdown");
+const drop1 = document.getElementById("drop1");
+console.log("tes")
+
+dropdown.addEventListener("click", function() {
+    drop1.classList.toggle("hidden");
+});
+
+
