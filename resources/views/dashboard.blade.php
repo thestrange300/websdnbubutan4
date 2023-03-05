3de235be7a40378e682485752788c7894cb23732
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-03-05 20:48:25 +0700

    added dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,72 @@
 <div class="px-6 pt-6 2xl:container">
     <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         <div class="md:col-span-2 lg:col-span-1" >
-            <div class="h-full py-6 px-6 rounded-xl border border-gray-200 bg-white">
-                <h5 class="text-xl text-gray-700">Newest Post</h5>
-                ini nampilin post terbaru dalam bentuk card lagi
+            <div class="h-full py-3 px-6 rounded-xl border border-gray-200 bg-white">
+                
+                <h5 class="text-2xl text-gray-800 font-bold lg:block">Postingan Terbaru</h5>
+
+                <div class="mt-2 mb-2">
+                    <div class="sm:flex sm:items-center sm:justify-between sm:space-x-5">
+                      <div class="flex items-center flex-1 min-w-0">
+                        <div class="mt-0 mr-0 mb-0 flex-1 min-w-0">
+                          <p class="text-base font-semibold text-gray-800 truncate">Bazaar olahan Sayur Hidroponik</p>
+                          <a href="#" class="inline-block">
+                            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800"> Ecopreneur </span>
+                          </a>
+                          <a href="#" class="inline-block">
+                            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800"> Ecoschool </span>
+                          </a>
+                        </div>
+                      </div>
+                        <a href="" class=" text-sm font-bold text-black transition-all
+                            duration-200 rounded-lg">Lihat</a>
+                    </div>
+                  </div>
+                  <hr/>
+                  
+                  <div class="mt-2 mb-2">
+                    <div class="sm:flex sm:items-center sm:justify-between sm:space-x-5">
+                      <div class="flex items-center flex-1 min-w-0">
+                        <div class="mt-0 mr-0 mb-0 flex-1 min-w-0">
+                          <p class="text-base font-semibold text-gray-800 truncate">Bazaar olahan Sayur Hidroponik</p>
+                          <a href="#" class="inline-block">
+                            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800"> Ecopreneur </span>
+                          </a>
+                          <a href="#" class="inline-block">
+                            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800"> Ecoschool </span>
+                          </a>
+                        </div>
+                      </div>
+                        <a href="" class=" text-sm font-bold text-black transition-all
+                            duration-200 rounded-lg">Lihat</a>
+                    </div>
+                  </div>
+                  <hr/>
+
+                  <div class="mt-2 mb-2">
+                    <div class="sm:flex sm:items-center sm:justify-between sm:space-x-5">
+                      <div class="flex items-center flex-1 min-w-0">
+                        <div class="mt-0 mr-0 mb-0 flex-1 min-w-0">
+                          <p class="text-base font-semibold text-gray-800 truncate">Bazaar olahan Sayur Hidroponik</p>
+                          <a href="#" class="inline-block">
+                            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800"> Ecopreneur </span>
+                          </a>
+                          <a href="#" class="inline-block">
+                            <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800"> Ecoschool </span>
+                          </a>
+                        </div>
+                      </div>
+                        <a href="" class=" text-sm font-bold text-black transition-all
+                            duration-200 rounded-lg">Lihat</a>
+                    </div>
+                  </div>
+                  <hr/>
+                
+                <div class="pb-1 pt-3 text-right">
+                    <a href="" class=" text-sm font-bold text-black transition-all duration-200 rounded-lg ">Selengkapnya</a> 
+                </div>
+                 
+
             </div>
         </div>
         <div>
@@ -23,28 +86,63 @@
             </div>
         </div>
         <div>
-            <div class="lg:h-full py-8 px-6 text-gray-600 rounded-xl border border-gray-200 bg-white">
-                <div class="mt-6">
-                    <h1 class="text-7xl text-center m-4">90</h1>
-                    <h5 class="text-xl text-gray-700 text-center">Jumlah Post Saat Ini</h5>
-                </div>
-                <table class="mt-6 -mb-2 w-full text-gray-600">
-                    <tbody>
-                        <tr>
-                            <td class="py-2">Kategori 1 terbanyak</td>
-                            <td class="text-gray-500">total berapa</td>
-                        </tr>
-                        <tr>
-                            <td class="py-2">kategori 2 terbanyak</td>
-                            <td class="text-gray-500">total berapa</td>  
-                        </tr>
-                        <tr>
-                            <td class="py-2">Kategori 3 terbanyak</td>
-                            <td class="text-gray-500">total berapa</td>
-                            </td>   
-                        </tr>
-                    </tbody>
-                </table>   
+            <div class="lg:h-full py-2 px-2 text-gray-600 rounded-xl border border-gray-200 bg-white" id="chart-container">
+                <script>
+                var dom = document.getElementById('chart-container');
+                var myChart = echarts.init(dom,'roma', {
+                renderer: 'canvas',
+                useDirtyRect: false
+                });
+                var app = {};
+
+                var option;
+
+                option = {
+                color: ['#80c2ff', '#2e9fdb', '#2179cc', '#00ffb7', '#00ff4c'],
+                title: {
+                    text: 'Total Post',
+                    left: 'center'
+                },
+                tooltip: {
+                    trigger: 'item'
+                },
+                legend: {
+                    orient: 'horizontal',
+                    left: 'center',
+                    bottom: '0%'
+                },
+                series: [
+                    {
+                    name: 'Pie Chart',
+                    type: 'pie',
+                    radius: '70%',
+                    data: [
+                        { value: 1048, name: 'Ekstra' },
+                        { value: 735, name: 'Adiwiyata' },
+                        { value: 580, name: 'Karya dan Prestasi' }
+                    ],
+                    //     label: {
+                    //     show: true,
+                    //     position: 'center'
+                    // },
+                    emphasis: {
+                        itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                    
+                    }
+                ]
+                };
+
+                if (option && typeof option === 'object') {
+                myChart.setOption(option);
+                }
+
+                window.addEventListener('resize', myChart.resize);
+                </script>
             </div>
         </div>
     </div>
--- resources/views/partial/main_navbar.blade.php
+++ resources/views/partial/main_navbar.blade.php
@@ -7,6 +7,7 @@
     {{-- <link href="{{ asset('/css/dropdown.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" /> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <script src="https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     
     <title>SDN Bubutan IV</title>
