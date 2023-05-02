d7a76c38551eec2d270d127d4664c5db498a3a87
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-05-02 22:10:10 +0700

    added detail post

--- resources/views/detailpost.blade.php
+++ resources/views/detailpost.blade.php
@@ -27,7 +27,7 @@
     <div class="mt-2 text-left">
         <p class="text-sm text-gray-500 py-2">Dipublikasikan dalam <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800"> Ecoschool </span> </p>
     </div>
-    <div class="flex items-left justify-left py-2">
+    <div class="flex items-left justify-left pt-1 pb-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-gray-600 w-5 h-5 mr-1">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
           </svg>
