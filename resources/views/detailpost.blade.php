552cebb2c6b419408e1eda5b3b67baf2ecdefb8a
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-05-03 09:26:22 +0700

    about fix, redesign detail post

--- resources/views/detailpost.blade.php
+++ resources/views/detailpost.blade.php
@@ -5,29 +5,32 @@
 
 <div class="mt-2">
     
-    <div class="place-item-center">
-        <h1 class="font-black text-center text-gray-800 md:text-5xl text-3xl py-6">Lomba Sayur Hidroponik</h1>
-            
-    </div>  
+ 
 
     <div class="relative z-0">
-        <div class="content-center px-12 pt-4">
-          <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+        <div class="content-center px-20 pt-4">
+          <div class="relative drop-shadow-md sm:rounded-2xl sm:overflow-hidden">
             <div class="relative inset-0">
               <img class="h-full w-full object-cover object-center max-h-128" src="https://tirainews.com/assets/berita/original/99738581944-img-20210928-wa0017.jpg" alt="Img Header">
             </div>
           </div>
         </div>
       </div>
+
+
      
 </div>
 
 
-<div class="drop-shadow-md content-center mt-8 mx-24 my-4 px-16 py-8 rounded-xl border border-gray-200 bg-white">
-    <div class="mt-2 text-left">
-        <p class="text-sm text-gray-500 py-2">Dipublikasikan dalam <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800"> Ecoschool </span> </p>
+<div class="drop-shadow-md content-center mx-24 my-4 px-16 pt-4 pb-8 rounded-xl border border-gray-200 bg-white">
+    <div class="place-item-left">
+        <h1 class="font-black text-left text-gray-800 md:text-4xl text-2xl pt-6">Lomba Sayur Hidroponik</h1> 
+    </div> 
+
+    <div class="text-left pt-2">
+        <p class="text-sm text-gray-500 pt-4">Dipublikasikan dalam <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-orange-100 text-orange-800"> Ecoschool </span> </p>
     </div>
-    <div class="flex items-left justify-left pt-1 pb-2">
+    <div class="flex items-left justify-left pt-2 pb-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-gray-600 w-5 h-5 mr-1">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
           </svg>
