7175a19c4869260b3eff7e74d8801233965d42eb
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-05-06 23:06:40 +0700

    pagination fix

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -169,53 +169,56 @@
         </div>
     </div>
 
-{{-- PAGINATION START --}}
-
-<nav aria-label="Page navigation example">
-    <ul class="list-style-none flex">
-      <li>
-        <a
-          class="pointer-events-none relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-500 transition-all duration-300"
-          >Previous</a
-        >
-      </li>
-      <li>
-        <a
-          class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
-          href="#!"
-          >1</a
-        >
-      </li>
-      <li aria-current="page">
-        <a
-          class="relative block rounded bg-primary-100 px-3 py-1.5 text-sm font-medium text-primary-700 transition-all duration-300"
-          href="#!"
-          >2
-          <span
-            class="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]"
-            >(current)</span
-          >
-        </a>
-      </li>
-      <li>
-        <a
-          class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
-          href="#!"
-          >3</a
-        >
-      </li>
-      <li>
-        <a
-          class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
-          href="#!"
-          >Next</a
-        >
-      </li>
-    </ul>
-  </nav>
-
-{{-- PAGINATION END --}}
-
     </div>
 </div>
+
+{{-- PAGINATION START --}}
+<div class="flex justify-center pb-5">
+    <nav aria-label="Page navigation example">
+        <ul class="list-style-none flex">
+          <li>
+            <a
+              class="relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
+              href="#!"
+              >Sebelumnya</a
+            >
+          </li>
+          <li>
+            <a
+              class="relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
+              href="#!"
+              >1</a
+            >
+          </li>
+          <li aria-current="page">
+            <a
+              class="relative block rounded bg-primary-100 px-3 py-1.5 text-base font-bold text-primary-700 transition-all duration-300"
+              href="#!"
+              >2
+              <span
+                class="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]"
+                >(current)</span
+              >
+            </a>
+          </li>
+          <li>
+            <a
+              class="relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
+              href="#!"
+              >3</a
+            >
+          </li>
+          <li>
+            <a
+              class="relative block rounded bg-transparent px-3 py-1.5 text-base text-neutral-600 transition-all duration-300 hover:bg-indigo-500 hover:text-white"
+              href="#!"
+              >Selanjutnya</a
+            >
+          </li>
+        </ul>
+      </nav>
+</div>
+
+
+{{-- PAGINATION END --}}
 @endsection
