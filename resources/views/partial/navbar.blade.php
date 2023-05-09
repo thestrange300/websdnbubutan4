16f3b7ed7c08973cbe84db4ce60f3118c725da64
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-05-09 23:48:45 +0700

    minor fix

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -6,7 +6,7 @@
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           
           <!-- Mobile menu button-->
-          <button type="button" class="pointer-events-auto inline-flex items-center justify-center p-2 rounded-md text-gray-400 text-white bg-transparent" aria-controls="mobile-menu" aria-expanded="false">
+          <button type="button" class="pointer-events-auto inline-flex items-center justify-center p-2 rounded-md text-gray-400 bg-transparent" aria-controls="mobile-menu" aria-expanded="false">
             <svg id="dropdown" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" color="black" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
