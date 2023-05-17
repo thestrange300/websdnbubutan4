0c93673697967fafeed7253cc49c2f6070c3685c
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-05-17 13:13:51 +0700

    minor fix

--- resources/views/dashboard_edit.blade.php
+++ resources/views/dashboard_edit.blade.php
@@ -39,10 +39,10 @@
             class="mb-3 mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-gradient-to-r from-sky-600 to-cyan-400  checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
             type="checkbox"
             role="switch"
-            id="switch_button">
+            id="switch_button" checked>
             <input type="text"
             id="switch_content" 
-            class="hidden form-control drop-shadow-md mt-2 mb-4 px-2 py-2 
+            class="form-control drop-shadow-md mt-2 mb-4 px-2 py-2 
             mt-1
             block
             w-full
@@ -58,6 +58,7 @@
         <script>
           const switchBtn = document.getElementById("switch_button");
           const switchctn = document.getElementById("switch_content");
+          const link = document.getElementById("id");
         
           switchBtn.addEventListener("change", () => {
             if (switchBtn.checked) {
