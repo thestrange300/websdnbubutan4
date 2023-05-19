c9cc3bb61a92608d423badc9981641b38b04437c
Auliya <liet60@gmail.com>
2023-05-19 17:51:29 +0700

    minor fix

--- resources/views/dashboard_edit.blade.php
+++ resources/views/dashboard_edit.blade.php
@@ -52,10 +52,10 @@
             class="mb-3 mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-gradient-to-r from-sky-600 to-cyan-400  checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
             type="checkbox"
             role="switch"
-            id="switch_button" checked>
+            id="switch_button">
             <input type="text"
             id="switch_content" 
-            class="form-control drop-shadow-md mt-2 mb-4 px-2 py-2 
+            class="hidden form-control drop-shadow-md mt-2 mb-4 px-2 py-2 
             mt-1
             block
             w-full
@@ -63,21 +63,26 @@
             border-gray-300
             shadow-sm
             focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-            max-w-lg" id="link" name="link" value="{{ old('link', $item->link) }}">  
+            max-w-lg" id="switch_content" name="link" value="{{ old('link', $item->link) }}">  
         </div>
 
         {{-- SCRIPT SWITCH --}}
 
         <script>
           const switchBtn = document.getElementById("switch_button");
-          const switchctn = document.getElementById("switch_content");
-          const link = document.getElementById("id");
+          const switchCtn = document.getElementById("switch_content");
+          const oldValue = "{{ old('link', $item->link) }}";
+        
+          if (oldValue) {
+            switchBtn.checked = true;
+            switchCtn.classList.remove("hidden");
+          }
         
           switchBtn.addEventListener("change", () => {
             if (switchBtn.checked) {
-              switchctn.classList.remove("hidden");
+              switchCtn.classList.remove("hidden");
             } else {
-              switchctn.classList.add("hidden");
+              switchCtn.classList.add("hidden");
             }
           });
         </script>
