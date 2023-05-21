a9b46e959cb3968d965ed0c730a62539c04d4dfd
thestrange300 <tegarsangkeyboardist@gmail.com>
2023-05-21 12:17:52 +0700

    dashboardpost fix

--- resources/views/dashboard_post.blade.php
+++ resources/views/dashboard_post.blade.php
@@ -123,9 +123,9 @@
               <table class="table-auto min-w-full divide-y divide-gray-300">
                 <thead class="bg-gray-50">
                   <tr>
-                    <th scope="col" class="py-3.5 text-left text-sm font-semibold text-gray-900 sm:pl-6">ID</th>
-                    <th scope="col" class="py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
-                    <th scope="col" class="py-3.5 text-left text-sm font-semibold text-gray-900">Content</th>
+                    <th scope="col" class="py-3.5 text-left pl-3 text-sm font-semibold text-gray-900 sm:pl-6">ID</th>
+                    <th scope="col" class="py-3.5 text-left pl-3 text-sm font-semibold text-gray-900">Title</th>
+                    <th scope="col" class="py-3.5 text-left pl-3 text-sm font-semibold text-gray-900">Content</th>
                     <th scope="col" class="py-4 px-8 text-left text-sm font-semibold text-gray-900">Action</th>
                   </tr>
                 </thead>
@@ -133,8 +133,8 @@
                   @foreach ($post as $item)
                   <tr>
                     <td class="whitespace-nowrap py-4 text-sm font-medium text-gray-900 sm:pl-6">{{ $item->id }}</td>
-                    <td class="whitespace-nowrap py-4 text-sm text-gray-500">{{ $item->judul }}</td>
-                    <td class="whitespace-nowrap text-sm text-gray-500 max-w-md">
+                    <td class="whitespace-normal py-4 pl-3 text-sm truncate text-gray-500">{{ $item->judul }}</td>
+                    <td class="whitespace-nowrap text-sm pl-3 text-gray-500 max-w-md">
                     <div class=" line-clamp-3">
                       {!! html_entity_decode($item->konten) !!}
                     </div>
@@ -153,7 +153,7 @@
                         data-te-target="#removemodal"
                         data-te-ripple-init
                         data-te-ripple-color="light"  
-                        onclick="getdel({{ $item->id }})"
+                        onclick="getdelete({{ $item->id }})"
                         class="inline-flex items-center px-2 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-red-600 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" >
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
